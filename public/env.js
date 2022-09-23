6433f57e78045f9d0e4bcb7652acb8bd98677fc2
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-09-23 21:58:09 +0200

    Delete env.js

--- public/env.js
+++
@@ -1,7 +0,0 @@
-window.env = {
-REACT_APP_API: "AIzaSyB2mOgeodybc2qjz7ZB7ZLW5Z_EsQOMZmA",
-REACT_APP_DOMAIN:"flashcard-app-5ddc9.firebaseapp.com",
-REACT_APP_PROJECT_ID:"flashcard-app-5ddc9",
-REACT_APP_BUCKET:"flashcard-app-5ddc9.appspot.com",
-REACT_APP_SENDER:93227632429,
-REACT_APP_APP_ID:"1:93227632429:web:5379f408513c551dd6b5a4"};
