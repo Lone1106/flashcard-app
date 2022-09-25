d927a8acb960a1cf64792435fc978316b3823a3e
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-09-25 16:33:02 +0200

    Fixed env

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2mOgeodybc2qjz7ZB7ZLW5Z_EsQOMZmA",
-  authDomain: "flashcard-app-5ddc9.firebaseapp.com",
-  projectId: "flashcard-app-5ddc9",
-  storageBucket: "flashcard-app-5ddc9.appspot.com",
-  messagingSenderId: "93227632429",
-  appId: "1:93227632429:web:5379f408513c551dd6b5a4"
+  apiKey: process.env.REACT_APP_API,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
